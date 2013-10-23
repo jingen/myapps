ccf3d47671af60ad0c590904e8e658135b3ec99c
Jingen Lin <jingen.lin.jl@gmail.com>
2013-10-23 13:08:04 -0400

    seo share/meta : 2013-10-23 13:08:04 -0400 Wednesday

--- app/controllers/partial_controller.rb
+++ app/controllers/partial_controller.rb
@@ -2,7 +2,6 @@ class PartialController < ApplicationController
   def partial
     if [
          "image_resize",
-         "image_result",
          "spell_check",
        ].include?(params[:partial])
       render params[:partial], :layout => false
